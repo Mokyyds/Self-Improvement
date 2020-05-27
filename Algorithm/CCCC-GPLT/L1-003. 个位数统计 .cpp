4f7bec0f86dd6b2773417e33fd9dbd8083aaeb10
Peach <taolei.zhou@ia.ac.cn>
2020-05-27 14:13:48 +0800

    Add Kotlin

---
+++ Algorithm/CCCC-GPLT/L1-003. 个位数统计 .cpp
@@ -0,0 +1,17 @@
+//
+// Created by Peach on 2020/5/27.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int len = s.length();
+    int a[10] = {0};
+    for (int i = 0; i < len; i++) a[s[i] - '0']++;
+    for (int i = 0; i < 10; i++) {
+        if (a[i] != 0) cout << i << ":" << a[i] << endl;
+    }
+    return 0;
+}
---
+++ Language/Kotlin/src/HelloWorld.kt
@@ -0,0 +1,12 @@
+fun main() {
+    runBlock {
+        List(1000) {
+            println("HelloWorld")
+        }
+    }
+}
+fun runBlock(block: ()-> Unit){
+    val start = System.currentTimeMillis()
+    block()
+    println(System.currentTimeMillis() - start)
+}
